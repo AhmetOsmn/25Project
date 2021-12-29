dcce19207738db8a9fe9e3b1700808ca0c3b0503
AhmetOsmn <Ahmet.osmn@outlook.com>
2021-12-29 22:42:30 +0300

    9.Project Added

---
+++ 9.TelefonRehber/Form1.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace _9.TelefonRehber
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.maskedTextBoxTel = new System.Windows.Forms.MaskedTextBox();
+            this.textBoxID = new System.Windows.Forms.TextBox();
+            this.buttonEkle = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxAd = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxSoyad = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxMail = new System.Windows.Forms.TextBox();
+            this.buttonSil = new System.Windows.Forms.Button();
+            this.buttonGuncelle = new System.Windows.Forms.Button();
+            this.buttonTemizle = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dataGridView1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(639, 338);
+            this.groupBox1.TabIndex = 30;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "REHBER";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.buttonTemizle);
+            this.groupBox2.Controls.Add(this.buttonGuncelle);
+            this.groupBox2.Controls.Add(this.buttonSil);
+            this.groupBox2.Controls.Add(this.textBoxMail);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.textBoxSoyad);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.textBoxAd);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.buttonEkle);
+            this.groupBox2.Controls.Add(this.textBoxID);
+            this.groupBox2.Controls.Add(this.maskedTextBoxTel);
+            this.groupBox2.Location = new System.Drawing.Point(657, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(331, 338);
+            this.groupBox2.TabIndex = 15;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "YENİ KİŞİ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID:";
+            // 
+            // maskedTextBoxTel
+            // 
+            this.maskedTextBoxTel.BackColor = System.Drawing.Color.PeachPuff;
+            this.maskedTextBoxTel.Location = new System.Drawing.Point(101, 141);
+            this.maskedTextBoxTel.Mask = "(999) 000-0000";
+            this.maskedTextBoxTel.Name = "maskedTextBoxTel";
+            this.maskedTextBoxTel.Size = new System.Drawing.Size(212, 26);
+            this.maskedTextBoxTel.TabIndex = 3;
+            // 
+            // textBoxID
+            // 
+            this.textBoxID.BackColor = System.Drawing.Color.PeachPuff;
+            this.textBoxID.Enabled = false;
+            this.textBoxID.Location = new System.Drawing.Point(101, 32);
+            this.textBoxID.Name = "textBoxID";
+            this.textBoxID.Size = new System.Drawing.Size(212, 26);
+            this.textBoxID.TabIndex = 15;
+            // 
+            // buttonEkle
+            // 
+            this.buttonEkle.Location = new System.Drawing.Point(101, 218);
+            this.buttonEkle.Name = "buttonEkle";
+            this.buttonEkle.Size = new System.Drawing.Size(104, 42);
+            this.buttonEkle.TabIndex = 5;
+            this.buttonEkle.Text = "EKLE";
+            this.buttonEkle.UseVisualStyleBackColor = true;
+            this.buttonEkle.Click += new System.EventHandler(this.buttonEkle_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 22);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(633, 313);
+            this.dataGridView1.TabIndex = 25;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 18);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "AD:";
+            // 
+            // textBoxAd
+            // 
+            this.textBoxAd.BackColor = System.Drawing.Color.PeachPuff;
+            this.textBoxAd.Location = new System.Drawing.Point(101, 68);
+            this.textBoxAd.Name = "textBoxAd";
+            this.textBoxAd.Size = new System.Drawing.Size(212, 26);
+            this.textBoxAd.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 109);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 18);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "SOYAD:";
+            // 
+            // textBoxSoyad
+            // 
+            this.textBoxSoyad.BackColor = System.Drawing.Color.PeachPuff;
+            this.textBoxSoyad.Location = new System.Drawing.Point(101, 106);
+            this.textBoxSoyad.Name = "textBoxSoyad";
+            this.textBoxSoyad.Size = new System.Drawing.Size(212, 26);
+            this.textBoxSoyad.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 144);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 18);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "TEL:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 175);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 18);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "MAİL:";
+            // 
+            // textBoxMail
+            // 
+            this.textBoxMail.BackColor = System.Drawing.Color.PeachPuff;
+            this.textBoxMail.Location = new System.Drawing.Point(101, 172);
+            this.textBoxMail.Name = "textBoxMail";
+            this.textBoxMail.Size = new System.Drawing.Size(212, 26);
+            this.textBoxMail.TabIndex = 4;
+            // 
+            // buttonSil
+            // 
+            this.buttonSil.Location = new System.Drawing.Point(211, 218);
+            this.buttonSil.Name = "buttonSil";
+            this.buttonSil.Size = new System.Drawing.Size(102, 42);
+            this.buttonSil.TabIndex = 6;
+            this.buttonSil.Text = "SİL";
+            this.buttonSil.UseVisualStyleBackColor = true;
+            this.buttonSil.Click += new System.EventHandler(this.buttonSil_Click);
+            // 
+            // buttonGuncelle
+            // 
+            this.buttonGuncelle.Location = new System.Drawing.Point(101, 266);
+            this.buttonGuncelle.Name = "buttonGuncelle";
+            this.buttonGuncelle.Size = new System.Drawing.Size(104, 42);
+            this.buttonGuncelle.TabIndex = 7;
+            this.buttonGuncelle.Text = "GÜNCELLE";
+            this.buttonGuncelle.UseVisualStyleBackColor = true;
+            this.buttonGuncelle.Click += new System.EventHandler(this.buttonGuncelle_Click);
+            // 
+            // buttonTemizle
+            // 
+            this.buttonTemizle.Location = new System.Drawing.Point(211, 266);
+            this.buttonTemizle.Name = "buttonTemizle";
+            this.buttonTemizle.Size = new System.Drawing.Size(104, 42);
+            this.buttonTemizle.TabIndex = 8;
+            this.buttonTemizle.Text = "TEMİZLE";
+            this.buttonTemizle.UseVisualStyleBackColor = true;
+            this.buttonTemizle.Click += new System.EventHandler(this.buttonTemizle_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MediumSpringGreen;
+            this.ClientSize = new System.Drawing.Size(1000, 362);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button buttonTemizle;
+        private System.Windows.Forms.Button buttonGuncelle;
+        private System.Windows.Forms.Button buttonSil;
+        private System.Windows.Forms.TextBox textBoxMail;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxSoyad;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxAd;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonEkle;
+        private System.Windows.Forms.TextBox textBoxID;
+        private System.Windows.Forms.MaskedTextBox maskedTextBoxTel;
+    }
+}
+
---
+++ 9.TelefonRehber/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace _9.TelefonRehber
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection connection = new SqlConnection("Data Source=DESKTOP-BHGN45D\\AHMETSDBSERVER;Initial Catalog=25Project-9.TelefonRehber;Integrated Security=True");
+        
+        void ListData()
+        {
+            DataTable dt = new DataTable();
+            SqlDataAdapter adapter = new SqlDataAdapter("Select * from Kisiler",connection);
+            adapter.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+
+        void ClearData()
+        {
+            textBoxAd.Text = "";
+            textBoxSoyad.Text = "";
+            textBoxID.Text = "";
+            textBoxMail.Text = "";
+            maskedTextBoxTel.Text = "";
+            textBoxAd.Focus();
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ListData();
+        }
+
+        private void buttonEkle_Click(object sender, EventArgs e)
+        {
+            connection.Open();
+            SqlCommand command = new SqlCommand("insert into Kisiler (Ad, Soyad, Telefon, Mail) values (@P1, @P2, @P3, @P4)",connection);
+            command.Parameters.AddWithValue("@P1", textBoxAd.Text);
+            command.Parameters.AddWithValue("@P2", textBoxSoyad.Text);
+            command.Parameters.AddWithValue("@P3", maskedTextBoxTel.Text);
+            command.Parameters.AddWithValue("@P4", textBoxMail.Text);
+            command.ExecuteNonQuery();
+            connection.Close();
+            MessageBox.Show("Kişi sisteme kaydedildi","Bilgi",MessageBoxButtons.OK,MessageBoxIcon.Information);
+            ListData();
+            ClearData();
+        }
+
+        private void buttonTemizle_Click(object sender, EventArgs e)
+        {
+            ClearData();
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int selectedRow = dataGridView1.SelectedCells[0].RowIndex;
+
+            textBoxID.Text = dataGridView1.Rows[selectedRow].Cells[0].Value.ToString();
+            textBoxAd.Text = dataGridView1.Rows[selectedRow].Cells[1].Value.ToString();
+            textBoxSoyad.Text = dataGridView1.Rows[selectedRow].Cells[2].Value.ToString();
+            maskedTextBoxTel.Text = dataGridView1.Rows[selectedRow].Cells[3].Value.ToString();
+            textBoxMail.Text = dataGridView1.Rows[selectedRow].Cells[4].Value.ToString();
+
+        }
+
+        private void buttonSil_Click(object sender, EventArgs e)
+        {
+            connection.Open();
+            SqlCommand command = new SqlCommand("Delete from Kisiler where ID=" + textBoxID.Text, connection);
+            command.ExecuteNonQuery();
+            connection.Close();
+            MessageBox.Show("Kisi rehberden silindi","Bilgi",MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            ListData();
+            ClearData();
+        }
+
+        private void buttonGuncelle_Click(object sender, EventArgs e)
+        {
+            connection.Open();
+            SqlCommand command = new SqlCommand("Update Kisiler set Ad=@P1, Soyad=@P2, Telefon=@P3, Mail=@P4   where ID=@P5", connection);
+            command.Parameters.AddWithValue("@P1",textBoxAd.Text);
+            command.Parameters.AddWithValue("@P2",textBoxSoyad.Text);
+            command.Parameters.AddWithValue("@P3",maskedTextBoxTel.Text);
+            command.Parameters.AddWithValue("@P4",textBoxMail.Text);
+            command.Parameters.AddWithValue("@P5",textBoxID.Text);
+            command.ExecuteNonQuery();
+            connection.Close ();
+            MessageBox.Show("Kişi bilgileri güncellendi","Uyarı", MessageBoxButtons.OK,MessageBoxIcon.Warning);
+            ListData();
+            ClearData();
+
+        }
+    }
+}
+//
