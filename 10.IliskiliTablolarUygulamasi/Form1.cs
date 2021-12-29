448612b644415fd27de5465e74de09acf862460c
AhmetOsmn <Ahmet.osmn@outlook.com>
2021-12-30 00:02:10 +0300

    10.Project Added

---
+++ 10.IliskiliTablolarUygulamasi/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _10.IliskiliTablolarUygulamasi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection connection = new SqlConnection("Data Source=DESKTOP-BHGN45D\\AHMETSDBSERVER;Initial Catalog=25Project-10.IliskiliTablolarUygulamasi;Integrated Security=True");
+        
+        void ListData()
+        {
+            DataTable dt = new DataTable();
+            SqlDataAdapter adapter = new SqlDataAdapter("SELECT h.HareketID, u.UrunAd as 'Ürün adı', (m.MusteriAd + ' '+ m.MusteriSoyad) as 'Müşteri', (p.PersonelAd + ' ' + p.PersonelSoyad) as 'Personel', h.Fiyat " + 
+                                                        "From Hareketler h "+
+                                                        "inner join Urunler u "+
+                                                        "on h.Urun = u.UrunID "+
+                                                        "inner join Musteriler m "+
+                                                        "on h.Musteri = m.MusteriID "+
+                                                        "inner join Personeller p "+
+                                                        "on h.Personel = p.PersonelID",connection);
+
+            adapter.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ListData();
+        }
+
+    }
+}
